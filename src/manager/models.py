aa2a5cefe256d9ff990d19c2effccfbbe9717022
Andrei <poniaable@gmail.com>
2023-07-31 22:51:06 +0300

    src

--- src/manager/models.py
+++ src/manager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-class
+class Book(models.Model):
+    class Meta:
+        verbose_name = 'книга'
+        verbose_name_plular = 'книга'
+
+    title = models.CharField
