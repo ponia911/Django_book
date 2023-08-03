ff325ab551c39bbca559fe445114b17aee8e1af6
Andrei <poniaable@gmail.com>
2023-08-03 18:01:37 +0300

    src

--- src/manager/models.py
+++ src/manager/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Book(models.Model):
     class Meta:
         verbose_name = 'книга'
-        verbose_name_plular = 'книга'
+        verbose_name_plular = 'книги'
 
-    title = models.CharField
+    title = models.CharField(max_length=100, verbose_name='название книги')
